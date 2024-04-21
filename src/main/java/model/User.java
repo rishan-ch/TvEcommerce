c4a5b2febf1f2513acad341af0b898b5a06a114c
Shreyash <shreyashghal3@gmail.com>
2024-04-21 09:20:54 +0545

    third commit

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,82 @@
 package model;
 
-public class Sample {
+import java.sql.Date;
+
+public class User {
+	// user variables and getters and setters are created
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private long phone;
+	private String role;
+	private Date dob;
+	private String gender;
+	private String address;
+	private String username;
+	private String password;
+	
+	
+	
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone2) {
+		this.phone = phone2;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
